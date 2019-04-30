4102b3d22d749a05c80be82d2703dfd82e22da15
FallingMountain <44987568+FallingMountain@users.noreply.github.com>
2019-04-30 08:25:00 -0500

    Update main.js

--- main.js
+++ main.js
@@ -270,4 +270,5 @@ document.getElementById("basicRocketLimit").innerHTML = game.rockLimit;
 document.getElementById("pUpgrade2Cost").innerHTML = game.pUp2cost;
 document.getElementById("pUpgrade3Cost").innerHTML = game.pUp3cost;
 document.getElementById("creativityMultiplier").innerHTML = game.creatMult;
+document.getElementById("moneyPerFuel").innerHTML = game.rock1.moneyPerFuel;
 }, 10);
